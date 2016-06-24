bae435b83fd52073287d4ba32f36245ed00fd60c
Abel Calderilla <Abel Calderilla>
2016-06-24 15:34:21 -0700

    Signed-off-by: Abel Calderilla <Abel Calderilla>

---
+++ Homework/HelloWorld_HW/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   HelloWorld Test
+ * Author: Abel Calderilla
+ *
+ * Created on June 23, 2016, 6:31 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World"<<endl;
+    
+    return 0;
+}
+
